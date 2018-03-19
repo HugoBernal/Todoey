1efa62a64a793059711177fcb296eef597dc1300
Hugo Bernal <hugonobernal@gmail.com>
2018-03-19 14:45:56 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Hugo Bernal on Mar/19/18.
+//  Copyright © 2018 Hugo Bernal. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    
+    var title: String = ""
+    
+    var done: Bool = false
+
+}
